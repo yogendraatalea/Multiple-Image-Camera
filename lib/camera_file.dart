ef9d04398d14ebbd258d2961e69052c3187af0d0
jyotiatalea <157569648+jyotiatalea@users.noreply.github.com>
2025-01-22 18:36:04 +0530

    Update camera_file.dart

--- lib/camera_file.dart
+++ lib/camera_file.dart
@@ -19,7 +19,7 @@ class _CameraFileState extends State<CameraFile> with TickerProviderStateMixin {
   double scale = 1.0;
   late List<CameraDescription> _cameras;
   CameraController? _controller;
-  List<XFile> imageFiles = [];
+  XFile? lastImageFile; // Changed to store only the last image
   List<MediaModel> imageList = <MediaModel>[];
   late int _currIndex;
   late Animation<double> animation;
@@ -27,9 +27,9 @@ class _CameraFileState extends State<CameraFile> with TickerProviderStateMixin {
   late AnimationController controller;
   late Animation<double> scaleAnimation;
 
-  addImages(XFile image) {
+  addImage(XFile image) { // Modified to handle single image
     setState(() {
-      imageFiles.add(image);
+      lastImageFile = image;
       _animationController = AnimationController(
           vsync: this, duration: const Duration(milliseconds: 1500));
       animation = Tween<double>(begin: 400, end: 1).animate(scaleAnimation =
@@ -42,178 +42,73 @@ class _CameraFileState extends State<CameraFile> with TickerProviderStateMixin {
 
   removeImage() {
     setState(() {
-      imageFiles.removeLast();
+      lastImageFile = null; // Clear the last image
     });
   }
 
-  Widget? _animatedButton({Widget? customContent}) {
-    return customContent != null
-        ? customContent
-        : Container(
-            height: 70,
-            width: 150,
-            decoration: BoxDecoration(
-              color: Colors.white38,
-              borderRadius: BorderRadius.circular(100.0),
-            ),
-            child: const Center(
-              child: Text(
-                'Done',
-                style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black),
-              ),
-            ),
-          );
-  }
+  // ... (keep other existing methods unchanged until _buildCameraPreview)
 
-  Future<void> _initCamera() async {
-    _cameras = await availableCameras();
-    // ignore: unnecessary_null_comparison
-    if (_cameras != null) {
-      _controller = CameraController(_cameras[0], ResolutionPreset.ultraHigh,
-          enableAudio: false);
-      _controller!.initialize().then((_) {
-        if (!mounted) {
-          return;
-        }
-        setState(() {});
-      });
-    } else {}
-  }
-
-  @override
-  void initState() {
-    _initCamera();
-    _currIndex = 0;
-
-    super.initState();
-  }
-
-Widget _buildCameraPreview() {
-  return GestureDetector(
-    onScaleStart: (details) {
-      zoom = _scaleFactor;
-    },
-    onScaleUpdate: (details) {
-      _scaleFactor = zoom * details.scale;
-      _controller!.setZoomLevel(_scaleFactor);
-    },
-    child: SizedBox(
-      width: double.infinity,
-      height: double.infinity,
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          CameraPreview(_controller!),
-          // Last capture thumbnail
-          Positioned(
-            left: 16,
-            bottom: 100,
-            child: Container(
-              alignment: Alignment.bottomLeft,
-              child: imageFiles.isNotEmpty
-                  ? ScaleTransition(
-                      scale: scaleAnimation,
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.push(
+  Widget _buildCameraPreview() {
+    return GestureDetector(
+        onScaleStart: (details) {
+          zoom = _scaleFactor;
+        },
+        onScaleUpdate: (details) {
+          _scaleFactor = zoom * details.scale;
+          _controller!.setZoomLevel(_scaleFactor);
+        },
+        child: SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+            child: Stack(fit: StackFit.expand, children: [
+              CameraPreview(_controller!),
+              if (lastImageFile != null) // Modified to show only last image
+                Positioned(
+                  left: 10,
+                  bottom: 100,
+                  child: ScaleTransition(
+                    scale: scaleAnimation,
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (BuildContext context) => ImagePreviewView(
-                                File(imageFiles.last.path),
-                                "",
+                                builder: (BuildContext context) =>
+                                    ImagePreviewView(
+                                      File(lastImageFile!.path),
+                                      "",
+                                    )));
+                      },
+                      child: Stack(
+                        children: [
+                          Image.file(
+                            File(lastImageFile!.path),
+                            height: 90,
+                            width: 60,
+                          ),
+                          Positioned(
+                            top: 0,
+                            right: 0,
+                            child: GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  removeImage();
+                                });
+                              },
+                              child: Image.network(
+                                "https://logowik.com/content/uploads/images/close1437.jpg",
+                                height: 30,
+                                width: 30,
                               ),
                             ),
-                          );
-                        },
-                        child: Stack(
-                          children: [
-                            Image.file(
-                              File(imageFiles.last.path),
-                              height: 90,
-                              width: 60,
-                            ),
-                            Positioned(
-                              top: 0,
-                              right: 0,
-                              child: GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    removeImage();
-                                  });
-                                },
-                                child: Image.network(
-                                  "https://logowik.com/content/uploads/images/close1437.jpg",
-                                  height: 30,
-                                  width: 30,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
+                          )
+                        ],
                       ),
-                    )
-                  : const SizedBox(),
-            ),
-          ),
-          // Camera switch button
-          Positioned(
-            right: MediaQuery.of(context).orientation == Orientation.portrait
-                ? 340
-                : null,
-            bottom: 0,
-            left: 0,
-            child: IconButton(
-              iconSize: 40,
-              icon: const Icon(
-                Icons.camera_front,
-                color: Colors.white,
-              ),
-              onPressed: _onCameraSwitch,
-            ),
-          ),
-          // Camera capture button
-          Positioned(
-            left: MediaQuery.of(context).orientation == Orientation.portrait
-                ? 0
-                : null,
-            bottom: MediaQuery.of(context).orientation == Orientation.portrait
-                ? 0
-                : MediaQuery.of(context).size.height / 2.5,
-            right: 0,
-            child: Column(
-              children: [
-                SafeArea(
-                  child: IconButton(
-                    iconSize: 80,
-                    icon: AnimatedSwitcher(
-                      duration: const Duration(milliseconds: 300),
-                      transitionBuilder: (child, anim) => RotationTransition(
-                        turns: child.key == const ValueKey('icon1')
-                            ? Tween<double>(begin: 1, end: 0.75).animate(anim)
-  Future<void> _onCameraSwitch() async {
-    final CameraDescription cameraDescription =
-        (_controller!.description == _cameras[0]) ? _cameras[1] : _cameras[0];
-    if (_controller != null) {
-      await _controller!.dispose();
-    }
-    _controller = CameraController(
-        cameraDescription, ResolutionPreset.ultraHigh,
-        enableAudio: false);
-    _controller!.addListener(() {
-      if (mounted) setState(() {});
-      if (_controller!.value.hasError) {}
-    });
-
-    try {
-      await _controller!.initialize();
-      // ignore: empty_catches
-    } on CameraException {}
-    if (mounted) {
-      setState(() {});
-    }
+                    ),
+                  ),
+                ),
+              // ... (keep the rest of the Stack children unchanged)
+            ])));
   }
 
   takePicture() async {
@@ -223,7 +118,7 @@ Widget _buildCameraPreview() {
     try {
       final image = await _controller!.takePicture();
       setState(() {
-        addImages(image);
+        addImage(image);
         HapticFeedback.lightImpact();
       });
     } on CameraException {
@@ -233,31 +128,17 @@ Widget _buildCameraPreview() {
 
   @override
   Widget build(BuildContext context) {
-    if (_controller != null) {
-      if (!_controller!.value.isInitialized) {
-        return Container();
-      }
-    } else {
-      return const Center(
-        child: SizedBox(
-          width: 32,
-          height: 32,
-          child: CircularProgressIndicator(),
-        ),
-      );
-    }
+    // ... (keep the existing build method unchanged until the actions list)
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         actions: [
-          imageFiles.isNotEmpty
+          lastImageFile != null // Modified condition
               ? GestureDetector(
                   onTap: () {
-                    for (int i = 0; i < imageFiles.length; i++) {
-                      File file = File(imageFiles[i].path);
-                      imageList.add(
-                          MediaModel.blob(file, "", file.readAsBytesSync()));
-                    }
+                    File file = File(lastImageFile!.path);
+                    imageList.add(
+                        MediaModel.blob(file, "", file.readAsBytesSync()));
                     Navigator.pop(context, imageList);
                   },
                   child: Padding(
@@ -276,14 +157,20 @@ Widget _buildCameraPreview() {
   }
 
   @override
-  void dispose() {
-    if (_controller != null) {
-      _controller!.dispose();
-    } else {
-      _animationController.dispose();
-    }
 
+  void dispose() {
+
+    if (_controller != null) {
+
+      _controller!.dispose();
+
+    } else {
+
+      _animationController.dispose();
+
+    }
     super.dispose();
+
   }
 }
 
