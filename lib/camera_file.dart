137b65c6c40111afe9eeed68ae92b3649977b9ea
Yogendra Kumar <59276348+yogendraatalea@users.noreply.github.com>
2025-01-30 17:52:14 +0530

    Update camera_file.dart

--- lib/camera_file.dart
+++ lib/camera_file.dart
@@ -214,22 +214,26 @@ class _CameraFileState extends State<CameraFile> with TickerProviderStateMixin {
                 }),
                 scrollDirection: Axis.horizontal,
               ),
-              Positioned(
-                // right:
-                //     MediaQuery.of(context).orientation == Orientation.portrait
-                //         ? 340
-                //         : null,
-                bottom: 0,
-                right: 0,
-                child: IconButton(
-                  iconSize: 40,
-                  icon: const Icon(
-                    Icons.camera_front,
-                    color: Colors.white,
+              Platform.isIOS
+              ? SafeArea(
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: IconButton(
+                          iconSize: 40,
+                          icon: const Icon(
+                            Icons.camera_front,
+                            color: Colors.white,
+                          ),
+                          onPressed: _onCameraSwitch,
+                        ),
+                      ),
+                    ],
                   ),
-                  onPressed: _onCameraSwitch,
-                ),
-              ),
+                )
+              : Container(),
               Positioned(
                 left: MediaQuery.of(context).orientation == Orientation.portrait
                     ? 0
